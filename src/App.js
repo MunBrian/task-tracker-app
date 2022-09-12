f3f88a9e606de91c0fe9393d834e3d52b8861904
Brian Mungai <bryanmungai254@gmail.com>
2022-09-12 14:20:45 +0300

    added toggle reminder action

--- src/App.js
+++ src/App.js
@@ -31,11 +31,16 @@ const deleteTask = (id) => {
   setTasks(tasks.filter(task => task.id !== id))
 }
 
+//toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
   return (
     <div className="container">
       <Header title='Task Tracker' />
       { 
-        tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask}/> : 'No Tasks available'
+        tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks available'
       }
     </div>
   );
