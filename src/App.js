57fa31e68860395b523f5d4bf681aebd3333949d
Brian Mungai <bryanmungai254@gmail.com>
2022-09-12 14:50:35 +0300

    added addTask component and addTask action

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from "react";
 
 const App= () => {
+  const [showAddTask. setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
   {
     id: 1,
@@ -36,9 +38,17 @@ const toggleReminder = (id) => {
   setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task))
 }
 
+//Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 1000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
   return (
     <div className="container">
       <Header title='Task Tracker' />
+      { showAddTask && <AddTask onAdd={addTask} />}
       { 
         tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks available'
       }
