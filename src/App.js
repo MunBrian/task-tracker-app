6583c5c1c3c277c23a7cd0d068d1a5e84656aca7
Brian Mungai <bryanmungai254@gmail.com>
2022-09-14 13:43:49 +0300

    added react-router-dom pckg, about and footer cmpnts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 //import React from "react";
 
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 
 const App= () => {
@@ -85,13 +88,27 @@ const addTask = async(task) => {
 }
 
   return (
-    <div className="container">
-      <Header title={"Task Tracker"} onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      { showAddTask && <AddTask onAdd={addTask} />}
-      { 
-        tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks available'
-      }
-    </div>
+    <Router >
+      <div className="container">
+        <Header title={"Task Tracker"} onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        <Routes>
+          <Route 
+            path="/" 
+            exact 
+            element={
+              <>
+                  { showAddTask && <AddTask onAdd={addTask} />}
+                  { 
+                    tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks available'
+                  }
+              </>
+            } 
+          />
+          <Route path="/about"  element={<About />}/>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
